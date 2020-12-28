044797766368ccea3f8e7489c87180c9ce567b59
elninokr <karimelnino69@gmail.com>
2020-12-28 13:25:33 -0500

    Completed all quizzes

--- TMA1/part2/script.js
+++ TMA1/part2/script.js
@@ -1,11 +1,14 @@
+// Ability to open the sidenav
 function openSideNav() {
   document.getElementById("mySidenav").style.width = "150px";
 }
 
+// Ability to open the sidenav
 function closeSideNav() {
   document.getElementById("mySidenav").style.width = "0px";
 }
 
+//reads local json file and returns it's content
 function readJSONquiz(file) {
   var req = new XMLHttpRequest();
   req.open("GET", file, false);
@@ -14,37 +17,38 @@ function readJSONquiz(file) {
   return jsonfile
 }
 
-function buildQuiz(questions) {
+//https://www.sitepoint.com/simple-javascript-quiz/
+function buildQuiz(quizQuestions, quizContainer) {
   // variable to store the HTML output
   const output = [];
 
   // for each question...
-  questions.forEach(
-    (currentQuestion, questionNumber) => {
+  quizQuestions.forEach(
+    (objQuestion, nbQuestion) => {
 
       // variable to store the list of possible answers
       const answers = [];
 
       // and for each available answer...
-      for (letter in currentQuestion.answers) {
+      for (choice in objQuestion.choices) {
 
         // ...add an HTML radio button
         answers.push(
           `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
+            <input type="radio" name="question${nbQuestion}" value="${choice}">
+            ${choice} :
+            ${objQuestion.choices[choice]}
+          </label>`
         );
       }
 
       // add this question and its answers to the output
       output.push(
         `<div class="slide">
-            <div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join("")} </div>
-            <div class="correct"></div>
-          </div>`
+          <div class="question"> ${objQuestion.question} </div>
+          <div class="answers"> ${answers.join("")} </div>
+          <div class="correct"></div>
+        </div>`
       );
     }
   );
@@ -53,90 +57,41 @@ function buildQuiz(questions) {
   quizContainer.innerHTML = output.join('');
 }
 
-function showSlide(n) {
-  slides[currentSlide].classList.remove('active-slide');
-  slides[n].classList.add('active-slide');
-  currentSlide = n;
-  if (currentSlide === 0) {
-    previousButton.style.display = 'none';
-  } else {
-    previousButton.style.display = 'inline-block';
-  }
-  if (currentSlide === slides.length - 1) {
-    nextButton.style.display = 'none';
-    submitButton.style.display = 'inline-block';
-  } else {
-    nextButton.style.display = 'inline-block';
-    submitButton.style.display = 'none';
-  }
-}
-
-function showNextSlide() {
-  showSlide(currentSlide + 1);
-}
-
-function showPreviousSlide() {
-  showSlide(currentSlide - 1);
-}
-
-function showResults(questions, quiz, results) {
+//https://www.sitepoint.com/simple-javascript-quiz/
+function showResults(quizQuestions, quizContainer, resultsContainer) {
 
   // gather answer containers from our quiz
-  const answerContainers = quiz.querySelectorAll('.answers');
-  const correctContainers = quiz.querySelectorAll('.correct');
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+  const correctContainers = quizContainer.querySelectorAll('.correct');
 
   // keep track of user's answers
   let numCorrect = 0;
 
   // for each question...
-  questions.forEach((currentQuestion, questionNumber) => {
+  quizQuestions.forEach((objQuestion, nbQuestion) => {
 
     // find selected answer
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
+    const answerContainer = answerContainers[nbQuestion];
+    const selector = `input[name=question${nbQuestion}]:checked`;
     const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
     // if answer is correct
-    if (userAnswer === currentQuestion.correctAnswer) {
+    if (userAnswer === objQuestion.answer) {
       // add to the number of correct answers
       numCorrect++;
 
       // color the answers green
-      answerContainers[questionNumber].style.color = 'lightgreen';
-      correctContainers[questionNumber].innerHTML = `Correct!`
+      answerContainers[nbQuestion].style.color = 'lightgreen';
+      correctContainers[nbQuestion].innerHTML = `Correct!`
     }
     // if answer is wrong or blank
     else {
       // color the answers red
-      answerContainers[questionNumber].style.color = 'red';
-      correctContainers[questionNumber].innerHTML = `Correct answer is ${currentQuestion.correctAnswer}`
+      answerContainers[nbQuestion].style.color = 'red';
+      correctContainers[nbQuestion].innerHTML = `Correct answer is ${objQuestion.answer}`
     }
   });
 
   // show number of correct answers out of total
-  results.innerHTML = `${numCorrect} / ${questions.length}`;
+  resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
 }
-
-// Variables
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-test = readJSONquiz("quizzes/quiz1.json");
-
-// Kick things off
-buildQuiz(test);
-
-// Pagination
-const previousButton = document.getElementById("previous");
-const nextButton = document.getElementById("next");
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
-
-// Show the first slide
-showSlide(currentSlide);
-
-// Event listeners
-submitButton.addEventListener('click', () => showResults(test, quizContainer, resultsContainer));
-previousButton.addEventListener("click", showPreviousSlide);
-nextButton.addEventListener("click", showNextSlide);
